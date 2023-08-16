2928cb24061ebe0087e163942c96101e838b5fb1
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-16 21:56:55 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <div>
+      <h2>Increment/Decrement Counter</h2>
+      <p>using react-redux</p>
+    </div>
+  );
+};
+
+export default App;
