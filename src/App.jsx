76992aa3748359a9c12508e2867bcc4aa2272f87
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-17 03:38:35 +0600

    dispatch and selector done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,18 @@
-import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decNumber, incNumber } from './components/actions/action';
 
 const App = () => {
+  const mystate = useSelector((state)=> state.changeTheNumber)
+  const dispatch = useDispatch()
   return (
     <div className='h-[100vh] flex justify-center items-center'>
       <div>
         <h2 className='text-4xl font-semibold mb-4'>Increment/Decrement Counter</h2>
         <p className='text-xl font-semibold text-center'>using react-redux</p>
         <div className='flex justify-center items-center mt-7 gap-5'>
-          <button className='btn font-bold'>-</button>
-          <p className='font-bold text-xl'>0</p>
-          <button className='btn font-bold'>+</button>
+          <button onClick={()=> dispatch(decNumber())}  className='btn font-bold'>-</button>
+          <p className='font-bold text-xl'>{mystate}</p>
+          <button onClick={()=> dispatch(incNumber())} className='btn font-bold'>+</button>
         </div>
       </div>
     </div>
