a170ff8f819b4fecc8a03b3f54e2ebb5d32019c0
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-16 22:02:38 +0600

    ui done

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,16 @@ import React from 'react';
 
 const App = () => {
   return (
-    <div>
-      <h2>Increment/Decrement Counter</h2>
-      <p>using react-redux</p>
+    <div className='h-[100vh] flex justify-center items-center'>
+      <div>
+        <h2 className='text-4xl font-semibold mb-4'>Increment/Decrement Counter</h2>
+        <p className='text-xl font-semibold text-center'>using react-redux</p>
+        <div className='flex justify-center items-center mt-7 gap-5'>
+          <button className='btn font-bold'>-</button>
+          <p className='font-bold text-xl'>0</p>
+          <button className='btn font-bold'>+</button>
+        </div>
+      </div>
     </div>
   );
 };
