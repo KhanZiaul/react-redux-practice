b5f28c2f5fd4fabd63761230dcab57e7fcba43bc
KHAN ZIAUL HASAN <khanzia16phy102@gmail.com>
2023-08-18 16:50:52 +0600

    reducer , store , subscribe and dispatch added

--- middleware/redux-2.js
+++ middleware/redux-2.js
@@ -1,7 +1,12 @@
 //  REDUX THUNK -------------------------
 
+const { combineReducers, createStore } = require("redux")
+
 // State
 
+const initialState = {
+    datas : []
+}
 
 // Action
 
@@ -27,13 +32,35 @@ const errorInApiData = (error) =>{
 
 // Reducer
 
+const apiReducer = (state = initialState , action) => {
+    switch(action.type) {
+        case "REQUEST_API_DATA" : return 
+        case "SUCCESSFULLY_GET_API_DATA" : return 
+        case "ERROR_IN_API_DATA" : return
+        default : return state
+    }
+}
+
 // Combine Reducer
+
+const rootReducer = combineReducers({apiReducer})
 
 // Redux MiddleWare
 
+function fetchData(){
+    
+}
+
 // Store
+
+const store = createStore(rootReducer)
 
 // Subscribe Store
 
+store.subscribe(()=>{
+    console.log(store.getState())
+})
 
 // Dispatch Store
+
+store.dispatch(fetchData())
